92d808e2c7042b6ebabd9b0b00ab3502f11d7c0d
rajumb0232 <rajugowda0232@gmail.com>
2025-05-18 12:40:31 +0530

    hyper links added to the hero section

--- src/lib/Explore.svelte
+++ src/lib/Explore.svelte
@@ -1,6 +1,6 @@
 <script></script>
 
-<section class="h-screen w-full flex flex-col justify-center items-center">
+<section id="explore" class="h-screen w-full flex flex-col justify-center items-center">
     <div>About me</div>
     <div>skills</div>
     <div>Projects</div>
--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -2,7 +2,7 @@
   import LinkedInSvg from "./svgs/LinkedIn_svg.svelte";
 </script>
 
-<div
+<section
   id="home"
   class="w-full h-screen flex flex-col md:flex-row justify-center items-center"
 >
@@ -20,12 +20,12 @@
       </p>
     </div>
     <div class="text-3xl mt-2.5 md:mt-0 flex flex-col items-start">
-      <p class="text-indigo-300 py-2">rajugowda.towork@gmail.com</p>
-      <span class="text-slate-500 ">GitHub</span>
+      <p class="text-indigo-300 py-2 hover:underline"><a href="mailto:rajugowda.towork@gmail.com">rajugowda.towork@gmail.com</a></p>
+      <span class="text-slate-500 hover:underline"><a target="_blank" href="https://github.com/rajumb0232">GitHub</a></span>
       <br />
-      <div class="mx-1 w-6 h-6">
-        <LinkedInSvg />
+      <div class="mx-1 w-6 h-6 hover:shadow-[0_0_20px_rgb(98,116,142)] transition-all duration-150">
+        <a href="www.linkedin.com/in/raju-gowda-dev"> <LinkedInSvg /></a>
       </div>
     </div>
   </div>
-</div>
+</section>
