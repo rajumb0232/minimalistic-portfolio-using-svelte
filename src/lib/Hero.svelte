76d84f192b5989411f9f039cbbf56356b2aaa6e1
rajumb0232 <rajugowda0232@gmail.com>
2025-05-18 16:42:31 +0530

    feat: add social media icons for LinkedIn, Hashnode, and blogs in Hero component

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,5 +1,24 @@
 <script>
-  import LinkedInSvg from "./svgs/LinkedIn_svg.svelte";
+  import hashnodeIcon from "../../public/socials/hashnode-icon.svg";
+  import linkedInIcon from "../../public/socials/linkedin-icon.svg";
+  import blogIcon from "../../public/socials/blog-icon.svg";
+  const socials = [
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/raju-gowda-dev",
+      icon: linkedInIcon,
+    },
+    {
+      name: "Hashnode",
+      url: "https://hashnode.com/@RajuGowda",
+      icon: hashnodeIcon,
+    },
+    {
+      name: "blogs",
+      url: "https://blog.rajugowda.in",
+      icon: blogIcon,
+    },
+  ];
 </script>
 
 <section
@@ -20,11 +39,31 @@
       </p>
     </div>
     <div class="text-3xl mt-2.5 md:mt-0 flex flex-col items-start">
-      <p class="text-indigo-300 py-2 hover:underline"><a href="mailto:rajugowda.towork@gmail.com">rajugowda.towork@gmail.com</a></p>
-      <span class="text-slate-500 hover:underline"><a target="_blank" href="https://github.com/rajumb0232">GitHub</a></span>
+      <p class="text-indigo-300 py-2 hover:underline">
+        <a href="mailto:rajugowda.towork@gmail.com"
+          >rajugowda.towork@gmail.com</a
+        >
+      </p>
+      <span class="text-slate-500 hover:underline"
+        ><a target="_blank" href="https://github.com/rajumb0232">GitHub</a
+        ></span
+      >
       <br />
-      <div class="mx-1 w-6 h-6 hover:shadow-[0_0_20px_rgb(98,116,142)] transition-all duration-150">
-        <a href="www.linkedin.com/in/raju-gowda-dev"> <LinkedInSvg /></a>
+
+      <div class="flex justify-center items-center bg-none">
+        {#each socials as { url, icon }}
+          <div
+            class="mr-6 w-7 h-7 bg-transparent rounded-sm hover:shadow-[0_0_20px_rgb(98,116,142)] transition-all duration-150"
+          >
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              <img
+                src={icon}
+                alt="Social Icon"
+                class="w-full h-full rounded-sm"
+              />
+            </a>
+          </div>
+        {/each}
       </div>
     </div>
   </div>
